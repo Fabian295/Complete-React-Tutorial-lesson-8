5fa3df536c22017fbce385b4de6197bb9c239099
Fabian295 <fabiankanhai70@gmail.com>
2022-12-10 03:51:40 +0100

    Add BlogList.js

---
+++ dojo-blog/src/BlogList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+const BlogList = (props) => {
+  return (
+    <div className='blog-list'>
+      <h2>{props.title}</h2>
+     {props.blogs.map((blog) => (
+        <div className="blog-preview"
+        key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default BlogList
